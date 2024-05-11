70f775c67d1bbafd85f1f1faaca1fb3385305325
unknown <sbrp2961@gmail.com>
2024-05-12 00:43:45 +0530

    first commit

---
+++ src/Sections/About/index.js
@@ -0,0 +1,174 @@
+import styled, { keyframes } from "styled-components";
+import wave from "../../assets/waves.svg";
+import rocket from "../../assets/rocket image.png";
+import human from "../../assets/human.svg";
+import hand from "../../assets/hand.svg";
+
+const move = keyframes`
+0% { transform: translateY(-5px)         }
+    50% { transform: translateY(10px) translateX(10px)        }
+    100% { transform: translateY(-5px)         }
+`;
+
+const AboutSection = styled.section`
+  width: 100vw;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Waves = styled.img`
+  width: 100%;
+  height: auto;
+  position: absolute;
+  top: -1rem;
+`;
+const Hand = styled.div`
+  position: absolute;
+  bottom: -1rem;
+  right: 0;
+
+  @media only Screen and (max-width: 40em) {
+    display: none;
+  }
+`;
+
+const Main = styled.div`
+  margin: 0 15rem;
+  margin-top: 15rem;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  @media only Screen and (max-width: 64em) {
+    margin: 0 calc(5rem + 5vw);
+    margin-top: 10rem;
+  }
+  @media only Screen and (max-width: 40em) {
+    align-items: center;
+    margin: 3rem calc(3rem + 3vw);
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  display: inline-block;
+`;
+
+const Title1 = styled.h3`
+  font-size: 1.3rem;
+  display: inline-block;
+  line-height: 1.4;
+`;
+
+const CurvedLine = styled.div`
+  width: 7rem;
+  height: 2rem;
+  border: solid 5px var(--purple);
+  border-color: var(--purple) transparent transparent transparent;
+  border-radius: 150%/60px 70px 0 0;
+`;
+
+const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  @media only Screen and (max-width: 40em) {
+    flex-direction: column;
+  }
+`;
+
+const Rocket = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  width: 40%;
+  padding-bottom: 5rem;
+  animation: ${move} 2.5s ease infinite;
+  @media only Screen and (max-width: 40em) {
+    width: 50vw;
+    padding-bottom: 0;
+  }
+`;
+
+const Human = styled.div`
+  width: 50%;
+  position: absolute;
+  right: 0;
+  bottom: 100%;
+
+  @media only Screen and (max-width: 40em) {
+    display: none;
+  }
+`;
+const Text = styled.h4`
+  font-size: calc(0.3rem + 1vw);
+  line-height: 1.4;
+  color: var(--nav2);
+`;
+const Circle = styled.span`
+  display: inline-block;
+  width: 1rem;
+  height: 1rem;
+  border-radius: 50%;
+  background-color: black;
+  margin-right: 0.5rem;
+  margin-top: 1rem;
+`;
+const AboutText = styled.div`
+  width: 50%;
+  position: relative;
+  @media only Screen and (max-width: 40em) {
+    width: 100%;
+  }
+`;
+
+const About = () => {
+  return (
+    <AboutSection id="about">
+      <Waves src={wave} alt="" />
+      <Hand>
+        <img src={hand} alt="" />
+      </Hand>
+      <Main>
+        <div>
+          <Title>Build a brighter financial future</Title>
+          <CurvedLine />
+        </div>
+        <Content>
+          <Rocket>
+            <img src={rocket} alt="" width="400" height="400" />
+          </Rocket>
+          <AboutText>
+            <Human>
+              <img src={human} alt="" width="400" height="400" />
+            </Human>
+            <div>
+              <Title1>Services:</Title1>
+            </div>
+             <Text>IVRS Ventures is an investment management firm that offers financial advice and guidance for both individuals and corporations.</Text>
+             <div>
+              <Title1>Focus:</Title1>
+            </div>
+            <Text>They create personalized financial plans (tailored solutions) to meet your specific goals.</Text>
+            <div>
+              <Title1>Expertise:</Title1>
+            </div>
+            <Text>Their team has extensive experience in the financial world and can provide valuable insights and strategic advice.</Text>
+            <div>
+              <Title1>Investment Strategy:</Title1>
+            </div>
+            <Text>Their primary concern is protecting your capital while aiming for a good return on investment. They focus on long-term growth to help you achieve your financial dreams.</Text>
+            {/* <div>
+              <Circle style={{ backgroundColor: "var(--purple)" }} />
+              <Circle style={{ backgroundColor: "var(--pink)" }} />
+              <Circle style={{ backgroundColor: "var(--black)" }} />
+            </div> */}
+          </AboutText>
+        </Content>
+      </Main>
+    </AboutSection>
+  );
+};
+
+export default About;
